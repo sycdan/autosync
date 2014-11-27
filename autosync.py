5a2f80a9297386552a46b3a4f14a949c0de8b41c
Dan Stace <dstace@gmail.com>
2014-11-27 14:07:23 -0500

    switched to multiprocessing to allow concurrent job syncs

--- autosync.py
+++ autosync.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import print_function
 from __future__ import unicode_literals
+from multiprocessing import Process
 import argparse
 import logging
 log = logging.getLogger(__name__)
@@ -10,6 +11,7 @@ import sys
 import re
 import json
 import subprocess
+import shutil
 from time import time, sleep
 from argparse import ArgumentParser
 from watchdog.observers import Observer
@@ -21,14 +23,25 @@ from watchdog.events import (
 )
 
 
-CONFIG_NAME = 'autosync.json'
-LOCK_NAME = 'autosync.lock'
-DELAY = 1.5  # seconds
 CWD = os.getcwd()
+CONFIG_FILE = os.path.join(CWD, 'autosync.json')
+WORK_DIR = os.path.join(CWD, '.autosync')
+DELAY = 1.5  # seconds
 
-config = {}
-handlers = {}
+# TODO: fix the fact that logging must be set up here instead of at the bottom so it can be used by subprocesses
+parser = argparse.ArgumentParser()
+group = parser.add_mutually_exclusive_group()
+group.add_argument('-v', '--verbose', help="increase verbosity (show debugging messages)",
+    action='store_const', const=logging.DEBUG, dest='loglevel')
+group.add_argument('-q', '--quiet', help="decrease verbosity (only show warnings)",
+    action='store_const', const=logging.WARNING, dest='loglevel')
+args = parser.parse_args()
 
+logging.basicConfig(
+    format="%(asctime)s %(levelname)-8s %(message)s",
+    datefmt="%H:%M:%S",
+    level=args.loglevel or logging.INFO
+)
 
 def fslash(path):
     return path.replace('\\', '/')
@@ -36,36 +49,42 @@ def fslash(path):
 def reslash(path):
     return path.replace('/', os.sep)
 
-class Job(object):
-    def __init__(self):
-        pass
-
-    def __repr__(self):
-        return "<Job: %s>" % self.name
-
-class Handler(FileSystemEventHandler):
+class Job(FileSystemEventHandler):
     changed_dirs = []
 
+    def __repr__(self):
+        return "<Job: {0}>".format(self.name)
+
     def __init__(self, observer, name, local, remote, ignore=[]):
-        log.debug("(%s) Creating handler", name)
-        super(Handler, self).__init__()
+        log.debug("Initializing job: %s", name)
+        super(Job, self).__init__()
         self.name = name
         self.last_change = None
         self.remote = remote
 
         self.local_rel = ".{0}{1}".format(os.sep, reslash(local))
         self.local_abs = os.path.abspath(local)
-        log.info("(%s) Local dir: %s (%s)", name, self.local_rel, self.local_abs)
 
-        self.lock_file = os.path.join(self.local_abs, LOCK_NAME)
-        if os.path.exists(self.lock_file):
-            log.debug("(%s) Removing old lock file: %s", name, self.lock_file)
-            os.remove(self.lock_file)
+        log.debug("%s Local rel: %s; Local abs: %s; Remote: %s", self, self.local_rel, self.local_abs, remote)
 
-        log.info("(%s) Remote dir: %s", name, remote)
+        self.work_dir = os.path.join(WORK_DIR, name)
+        if os.path.exists(self.work_dir):
+            log.debug("%s Deleting old work directory: %s", self, self.work_dir)
+            shutil.rmtree(self.work_dir)
+
+        attempts = 0
+        while True:
+            if attempts >= 10:
+                raise Exception("Couldn't create directory: {0}".format(self.work_dir))
+            log.debug("%s Creating work directory: %s (%s failed attempts)", self, self.work_dir, attempts)
+            try:
+                os.makedirs(self.work_dir)
+                break
+            except Exception as e:
+                attempts += 1
+                log.warning(e)
 
         # build a regular expression for the ignore list
-        log.info("(%s) Ignore list: %s", name, ignore)
         ignore_list = global_ignore + ignore
         for i, path in enumerate(ignore_list):
             if path.startswith('/'):
@@ -75,11 +94,18 @@ class Handler(FileSystemEventHandler):
             path = path.replace('.', '\.')
             path = path.replace('*', '.*')
             ignore_list[i] = path
-        log.debug("(%s) Regexified ignore list: %s", name, ignore_list)
+        log.debug("%s Regexified ignore list: %s", self, ignore_list)
         self.ignore_re = re.compile('|'.join(ignore_list), re.IGNORECASE)
 
-        log.debug("(%s) Adding handler to observer", name)
+        log.debug("%s Adding handler to observer", self)
         observer.schedule(self, self.local_abs, recursive=True)
+
+        log.debug("%s Watching queue: %s", self, self.work_dir)
+        self.queue_watcher = Process(target=self.watch_queue)
+        self.queue_watcher.start()
+
+        log.info("%s %s --> %s (ignoring %s)",
+            self, self.local_abs, remote, 'only globals' if not ignore else ', '.join(ignore))
 
     def on_created(self, event):
         if isinstance(event, FileCreatedEvent):# we don't care about dir mutations
@@ -94,21 +120,20 @@ class Handler(FileSystemEventHandler):
             self.handle_event(event)
 
     def handle_event(self, event):
-        if os.path.basename(event.src_path) == LOCK_NAME: return
         abs_path = abspath(event.src_path)
         abs_dir = os.path.dirname(abs_path)
         rel_path = relpath(abs_path, self.local_abs)
         rel_dir = os.path.dirname(rel_path)
-        if rel_dir == '.': rel_dir = ''
 
         if self.ignore_re.match(rel_path):
             log.debug("(%s) Ignored file changed: %s", self.name, abs_path)
             return
 
-        """log.debug(
+        log.debug(
             "(%s) New event:-\nAP: %s\nAD: %s\nRP: %s\nRD: %s",
             self.name, abs_path, abs_dir, rel_path, rel_dir
-        )"""
+        )
+
 
         self.last_change = time()
 
@@ -120,57 +145,114 @@ class Handler(FileSystemEventHandler):
         log.debug("(%s) Dir added to sync queue: %s", self.name, rel_dir)
         self.changed_dirs.append(rel_dir)
 
+    def write_queue(self, items=None):
+        """Write the list of dirs to include in a sync to a file in the work dir."""
+
+        # pop elements off the list of changed dirs until it's empty, in case more are added while we're working
+        
+        changed_dirs = []
+        while self.changed_dirs:
+            path = self.changed_dirs.pop()
+            if path not in changed_dirs:
+                changed_dirs.append(path)
+        log.info("%s Synchronizing changes in %s", self, ', '.join(changed_dirs))
+
+        # get the last item in the queue, if any, so we can name the next one
+        last_queue_num = 0
+        queue_files = self.queue_files()
+        if len(queue_files):
+            # convert back to a number
+            last_queue_num = int(os.path.splitext(os.path.basename(queue_files[-1]))[0])
+        self.last_change = 0
+        log.debug("%s Last queue item: %s", self, last_queue_num)
+        queue_file = os.path.join(self.work_dir, "{0}".format(last_queue_num + 1))
+
+        lines = []
+        for d in changed_dirs:
+            # write all parents, with no trailing slashes
+            head = d
+            while True:
+                head, tail = os.path.split(head)
+                if head == '': break
+                lines.append('{0}\n'.format(head))
+                if tail == '': break
+
+            # write the dir itself and tell rsync to include all files in it
+            lines.append('{0}{1}***\n'.format(d, '/'))
+        log.debug("%s Queue file lines: %s", self, lines)
+
+        with open(queue_file, 'wb') as f:
+            f.write('\n'.join(lines).encode('utf-8'))
+
+    def queue_files(self):
+        """
+        Returns the files in the queue dir as a sorted list.
+        Files are named as integers, with the lowest number being the highest priority.
+        """
+        ret = []
+        files = sorted(os.listdir(self.work_dir))
+        for file in files:
+            path = os.path.join(self.work_dir, file)
+
+            # skip dirs
+            if not os.path.isfile(path):
+                continue
+
+            # check it's a queue file valid name (can be cast to int)
+            try:
+                num = int(file)
+            except ValueError:
+                continue
+
+            ret.append(path)
+        return ret
+
+    def watch_queue(self):
+        """
+        Monitors the queue dir for lists of dirs to sync, and calls rsync when necessary.
+        Started as a subprocess when the job is created.
+        """
+        try:
+            while True:
+                sleep(0.5)
+                try:
+                    queue_file = self.queue_files().pop()
+                except IndexError:  # the queue was empty
+                    continue
+                args = [
+                   'rsync',
+                   '-vzru',
+                   '--delete',
+                   '--include-from={0}'.format(queue_file),
+                   '--exclude=*',
+                   '{0}/'.format(fslash(self.local_rel)),
+                   '{0}/'.format(self.remote)
+                ]
+                log.debug("%s Running command: %s", self, ' '.join(args))
+                start_time = time()
+                subprocess.call(args)
+                log.info("%s Sync done in %d seconds", self, time() - start_time)
+
+                log.debug("%s Removing queue file: %s", self, queue_file)
+                os.remove(queue_file)
+        except KeyboardInterrupt:
+            log.debug("%s Stopped watching queue", self)
+
+
 def tick():
-    for name, handler in handlers.items():
-        if handler.last_change is None:
-            continue
+    """Check each job to see if it's ready to sync."""
+    for _, job in jobs.items():
+        if job.last_change and time() > job.last_change + DELAY:
+            job.write_queue()
 
-        remaining_time = handler.last_change + DELAY - time()
-        if remaining_time > 0:
-            #log.debug("(%s) Delay not yet elapsed (%s to go)", name, remaining_time)
-            continue
-
-        # lock in the list of dirs to sync
-        dirs = list(handler.changed_dirs)
-        handler.changed_dirs = []
-        log.info("(%s) Synchronizing changes in %s", name, ', '.join(dirs))
-
-        # write the list of dirs to include to the lock file
-        with open(handler.lock_file, 'wb') as f:
-            for d in dirs:
-                # write all parents, with no trailing slashes
-                head = d
-                while True:
-                    head, tail = os.path.split(head)
-                    if head == '': break
-                    f.write('{0}\n'.format(head).encode('utf-8'))
-                    if tail == '': break
-
-                # write the dir itself and tell rsync to include all files in it
-                f.write('{0}{1}***\n'.format(d, '/').encode('utf-8'))
-
-        with open(handler.lock_file, 'rb') as f:
+        """
+        with open(job.lock_file, 'rb') as f:
             contents = f.read()
             log.debug("(%s) rsync include list:\n%s", name, contents)
 
-        args = [
-           'rsync',
-           '-vzru',
-           '--delete',
-           '--exclude=*',
-           '--include-from={0}'.format(relpath(handler.lock_file, CWD)),
-           '{0}/'.format(handler.local_rel),
-           '{0}/'.format(handler.remote)
-        ]
-        log.debug("Command to run: %s", ' '.join(args))
-        subprocess.call(args)
-        log.info("Sync done")
-
-        log.debug("Removing lock file: %s", handler.lock_file)
-        os.remove(handler.lock_file)
-
         log.debug("(%s) Resetting change time", name)
-        handler.last_change = None
+        job.last_change = None
+        """
 
 def abspath(path, resolve_links=False):
     path = os.path.expanduser(path)
@@ -184,68 +266,54 @@ def relpath(path, start):
 
 def load_config():
     """Look for a config file in the CWD and parse it."""
-    global config, global_ignore
+    global global_ignore
 
-    config_path = fslash(os.path.join(CWD, CONFIG_NAME))
-    if os.path.exists(config_path):
-        log.info("Loading config from %s", config_path)
-        with open(config_path, 'r') as config_file:
+    if os.path.exists(CONFIG_FILE):
+        log.info("Loading config from %s", CONFIG_FILE)
+        with open(CONFIG_FILE, 'r') as config_file:
             config = json.load(config_file)
         log.debug("Config dict: %s", config)
 
         # get the global ignore list
         global_ignore = config.get('ignore', [])
         log.info("Global ignore list: %s", global_ignore)
+        return config
     else:
         log.warning("No config file found in %s", CWD)
         sys.exit("Create %s first" % CONFIG_NAME)
 
-def create_handlers():
+def create_jobs(config):
     """Instantiates an event handler for each job defined in the config."""
-    global handlers, observer
-
-    jobs = config.get('jobs', [])
-    job_count = len(jobs)
+    jobs = {}
+    jobs_ = config.get('jobs', {})
+    job_count = len(jobs_)
     if job_count:
-        observer = Observer()
-        log.debug("Created observer %s", observer)
-        log.info("Setting up %s jobs (%s)", job_count, ', '.join(jobs.keys()))
-        handlers = {}
-        for name, data in jobs.items():
-            handler = Handler(observer, name, **data)
-            handlers[name] = handler
-        del jobs, job_count
+        log.debug("Starting %s jobs (%s)", job_count, ', '.join(jobs_.keys()))
+        for name, data in jobs_.items():
+            job = Job(observer, name, **data)
+            jobs[name] = job
+
+        del jobs_, job_count
         try:
             observer.start()
         except FileNotFoundError as e:
             log.error(e.args[1])
             sys.exit(1)
-        log.info("Started watching directories")
+        return jobs
     else:
         sys.exit("No jobs defined in config file")
 
-def main(args):
-    logging.basicConfig(
-        format="%(asctime)s %(levelname)-8s %(message)s",
-        datefmt="%H:%M:%S",
-        level=args.loglevel or logging.INFO
-    )
-    load_config()
-    create_handlers()
+if __name__ == '__main__':
+    if not os.path.exists(WORK_DIR):
+        os.makedirs(WORK_DIR)
+
+    observer = Observer()
+    config = load_config()
+    jobs = create_jobs(config)
     try:
         while True:
-            sleep(1)
             tick()
     except KeyboardInterrupt:
         observer.stop()
+        log.info("Normal shutdown")
     observer.join()
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('-v', '--verbose', help="increase verbosity (show debugging messages)",
-        action='store_const', const=logging.DEBUG, dest='loglevel')
-    group.add_argument('-q', '--quiet', help="decrease verbosity (only show warnings)",
-        action='store_const', const=logging.WARN, dest='loglevel')
-    args = parser.parse_args()
-    main(args)
